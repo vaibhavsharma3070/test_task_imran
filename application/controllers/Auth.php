e676ff0148ec7b910aa79874ed74a17545ce1f23
vaibhavsharma <vaibhavsharma3070@gmail.com>
2022-03-17 16:31:50 +0530

    register and login of a user

---
+++ application/controllers/Auth.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	/* Display login page */
+	public function index()
+	{
+		$this->load->view('login');
+	}
+
+	/* Display register page */
+	public function register(){
+		$this->load->view('register');
+	}
+
+	/* Register action to save data */
+	public function postregister(){
+
+		/* retrieve all details of form */
+		$name = $this->input->post('name');
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		/* Check email already  exists OR not in Db */
+		$this->db->where('email', $email);
+		$data = $this->db->get('users')->result();
+
+		/* fi exists sent back to register page */
+		if(count($data) >=1 ){
+			$this->session->set_flashdata('error', 'Oops!! Email already exists....');
+			redirect('/register');
+		}
+
+		/* Encrypt password for security */ 
+		$encryptedpass = password_hash($password, PASSWORD_DEFAULT);
+		
+
+		/* create array for insert data in database */
+		$data = ["name" => $name, "email" => $email , "password"=> $encryptedpass];
+
+		/* try and catch for insert and display any errors*/
+		try{
+			if($this->db->insert('users', $data)){
+				$this->session->set_flashdata('success', 'You have registered successfully. Please verify your email address.');
+				redirect('/');   
+			}	
+		} catch(Exception $e){
+			$this->session->set_flashdata('error', 'Something went wrong!! Please try again later');
+			redirect('/register');
+		}
+	}	
+
+	public function postlogin(){
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+
+			$this->db->where('email', $email);
+			$data = $this->db->get('users')->result();
+
+			if(count($data) == 1){
+				$verifyPasssword = password_verify($password, $data[0]->password); 	
+				if($verifyPasssword == 1){
+					 $data = ['email' => $data[0]->email, 'id' => $data[0]->id, 'name' => $data[0]->name, 'is_logged_in'=> '1'];
+					 $this->session->set_userdata($data);
+					 redirect('/products');	 
+				}
+			
+			} else{
+				$this->session->set_flashdata('error', 'Credenatials not found in system...');
+				redirect('/');
+			}
+
+			
+	}
+}
---
+++ application/views/products.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products</title>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <style type="text/css">
+        img {
+            height: 170px;
+            width: 140px
+        }
+  </style>
+</head>
+<body>
+
+    <div class="container">
+    <div class="row text-center text-white">
+        <div class="col-lg-3 m-0">
+            <h1 class="display-4">Product List</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+            <!-- List group-->
+            <ul class="list-group shadow">
+                <!-- list group item-->
+                <li class="list-group-item">
+                    <!-- Custom content-->
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5 class="mt-0 font-weight-bold mb-2">Apple iPhone XR (Red, 128 GB)</h5>
+                            <p class="font-italic text-muted mb-0 small">128 GB ROM | 15.49 cm (6.1 inch) Display 12MP Rear Camera | 7MP Front Camera A12 Bionic Chip Processor</p>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6 class="font-weight-bold my-2">₹64,999</h6>
+                                <ul class="list-inline small">
+                                    <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                                    <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                                    <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                                    <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                                    <li class="list-inline-item m-0"><i class="fa fa-star-o text-gray"></i></li>
+                                </ul>
+                            </div>
+                        </div><img src="https://i.imgur.com/KFojDGa.jpg" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2">
+                    </div> <!-- End -->
+                </li> <!-- End -->
+                <!-- list group item-->
+                
+            </ul> <!-- End -->
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
